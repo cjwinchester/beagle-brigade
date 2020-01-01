e885c840f9ea3d8cad35c7123bcc669ad89a0ba2
Cody Winchester <cjwinchester@gmail.com>
2020-01-01 15:38:14 -0700

    first push

---
+++ scripts.js
@@ -0,0 +1,57 @@
+/*
+  Also an excuse to play around with lazy image loading -- guidance from:
+    👉 https://www.smashingmagazine.com/2019/05/hybrid-lazy-loading-progressive-migration-native/
+    👉 https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video
+    👉  https://css-tricks.com/preventing-content-reflow-from-lazy-loaded-images/
+*/
+document.addEventListener('DOMContentLoaded', function() {
+
+  // grab an iterable array of images
+  var lazyImages = [].slice.call(document.getElementsByClassName('lazy'));
+
+  if ('IntersectionObserver' in window) {
+    var lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove('lazy');
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+
+  // if no intersection observer, fall back to polling for position
+  } else {
+    var active = false;
+    var lazyLoad = function() {
+      if (active === false) {
+        active = true;
+        setTimeout(function() {
+          lazyImages.forEach(function(lazyImage) {
+            if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== 'none') {
+              lazyImage.src = lazyImage.dataset.src;
+              lazyImage.classList.remove('lazy');
+              lazyImages = lazyImages.filter(function(image) {
+                return image !== lazyImage;
+              });
+              if (lazyImages.length === 0) {
+                document.removeEventListener("scroll", lazyLoad);
+                window.removeEventListener("resize", lazyLoad);
+                window.removeEventListener("orientationchange", lazyLoad);
+              }
+            }
+          });
+          active = false;
+        }, 500);
+      }
+    };
+    document.addEventListener('scroll', lazyLoad);
+    window.addEventListener('resize', lazyLoad);
+    window.addEventListener('orientationchange', lazyLoad);
+  }
+});
